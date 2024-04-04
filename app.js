b3a860b8f094230c23f1a8881eac4e731103a341
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-04-03 21:50:33 -0700

    Some small changes

--- app.js
+++ app.js
@@ -3,18 +3,27 @@
 
 // DONT FORGET TO LINK THEM TOGETHER THIS TIME 
 
-const playerInv = {
-    item: 0,
-}
+// const playerInv = {
+//     item: 0,
+// }
 
 
+var questions = {
+    'buy sword': 'here you go',
+    'sell fish': 'thank you'
+  }
+  function askNPC(question){
+    if(typeof questions[question] !== "undefined"){
+      return questions[question];
+    } else {
+      return 'Did not understand you question!';
+    }
+  }
+  
+  var answer = askNPC('buy sword');
+  var answer = askNPC('sell fish');
 
-
-
-
-
-
-
+console.log(askNPC)
 
 
 
