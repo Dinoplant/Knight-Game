16cb844a9dfb34268b44c376ea9a998d169abdef
Dinoplant <143848581+Dinoplant@users.noreply.github.com>
2024-04-08 15:44:49 -0700

    Tried to get the story into the js

--- app.js
+++ app.js
@@ -1,29 +1,79 @@
 
-// will find a way soon
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('btnOptions')
 
-// DONT FORGET TO LINK THEM TOGETHER THIS TIME 
+let player = {}
 
-// const playerInv = {
-//     item: 0,
-// }
-
-
-var questions = {
-    'buy sword': 'here you go',
-    'sell fish': 'thank you'
+function startGame() {
+ player = {
+    wis: 0,
+    str: 0,
+    con: 0,
+    dex: 0,
+    bow: 0,
+    rapier: 0,
+    greatAxe: 0,
+    spear: 0,
+    shortSword: 0,
+    healPot: 0,
+    jade: 0,
   }
-  function askNPC(question){
-    if(typeof questions[question] !== "undefined"){
-      return questions[question];
-    } else {
-      return 'Did not understand you question!';
+}
+
+function showTextNode(index) {
+  const textNode = textNodes.find(textNode => textNode.id === index)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('btn')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
     }
+  })
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
   }
-  
-  var answer = askNPC('buy sword');
-  var answer = askNPC('sell fish');
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
 
-console.log(askNPC)
+const textNodes = [
+{
+  id:1,
+  text: `hello`,
+  options: [
+    {
+      text: `heelllooo`,
+      setplayer: {str: 2,},
+      nextText: 2
+    }
+    {
+      text: `byeeee`,
+      setplayer: {wis: 2,},
+      nextText: 2
+    }
+  ]
+}
+
+]
 
 
 
+
+
+
+startGame ()
