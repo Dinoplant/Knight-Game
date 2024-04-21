05576b06f884b3dac3841564f82d77952eb74b49
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-04-20 20:23:30 -0700

    30% done witht he combat system
    Added dying, more than one enemy, fight at two different points, and the textnode changing after battle

--- app.js
+++ app.js
@@ -14,27 +14,25 @@ let continueCombat = false
 let playerHp = 150
 let damage = 0
 let attack = false
+let enemyD20 = 1
+let enemy1 = 0
+let enemy2 = 0
+let enemy3 = 0
+let endingNode = 0
 
-class lvl1Enemy {
-  constructor(enemyName, enemyHp, enemyPeace) {
+class Enemy {
+  constructor(enemyName, enemyHp, enemyPeace, extraDamage, extraRoll) {
     this.enemyName = enemyName;
     this.enemyHp = enemyHp;
     this.enemyPeace = enemyPeace;
-  }
-}
-
-
-class lvl2Enemy extends lvl1Enemy {
-  constructor(enemyHp, enemyName, enemyPeace, extraDamage, extraRoll) {
-    super(enemyHp, enemyName, enemyPeace);
     this.extraDamage = extraDamage;
     this.extraRoll = extraRoll;
   }
 }
-let Bandit = new lvl1Enemy (`Bandit`, 20, 10);
-let King = new lvl2Enemy (`King`, 100, 0, 30, 2)
 
-console.log(Bandit)
+let None = new Enemy(`Nothing`, 0, 0, 0, 0)
+let Bandit = new Enemy(`Bandit`, 20, 10, 0, 0);
+let King = new Enemy(`King`, 100, 0, 30, 0)
 
 // bandit bear, wolves, genral, Henry, gruad, archer, dragon, yourself, 
 // gernal, graud, archer, bear,  henry, dragoon, 
@@ -51,6 +49,7 @@ let player = {
   shortSword: false,
   healPot: 0,
   jade: 0,
+  debug: 0,
 }
 
 function startGame() {
@@ -64,25 +63,32 @@ function startGame() {
     greatAxe: 0,
     spear: 0,
     shortSword: 0,
-    healPot: 0,
+    healPot: 10,
     jade: 0,
+    debug: 0,
   }
   showTextNode(1)
-
 }
 
 function showTextNode(textNodeIndex) { // goes through tthe text nodes checks what I put for the text and changes the text in the HTML
   const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
   textElement.innerText = textNode.text
-  if (textNode.startCombat == 1) { //just checks is a paramantor to see if combat starts
-    startCombat(Bandit, Bandit, false, 0)
+  if (textNode.startCombat === 1) { //just checks is a paramantor to see if combat starts
     console.log('combat mode engaged');
-    combatQuestion = true
+    combatQuestion = false
+    startCombat()
+    combat(1, 3, 3)
+    endingNode = 8
+  }
+  if (textNode.startCombat === 2) { //just checks is a paramantor to see if combat starts
+    console.log('combat mode engaged');
+    combatQuestion = false
+    startCombat()
+    combat(1, 3, 2, 1)
   }
   else if (textNode.continueCombat === true) { //checks if you are continue combat
     console.log('combat mode cont');
     combatQuestion = true
-
   }
   else {
     console.log('no violence = sad pandas');
@@ -98,23 +104,17 @@ function showTextNode(textNodeIndex) { // goes through tthe text nodes checks wh
       button.innerText = option.text
       button.classList.add('btn')
       button.addEventListener('click', () => selectOption(option))
-
       if (option === textNode.options[0]) {
         button.addEventListener('click', () => slash()) //checking certain buttons and adding the comabt funation accordin to each one
-      }
-      else if (option === textNode.options[1]) {
+      } else if (option === textNode.options[1]) {
+        button.addEventListener('click', () => healPot(player.healPot))
+      } else if (option === textNode.options[2]) {
         button.addEventListener('click', () => slash())
-      }
-      else if (option === textNode.options[2]) {
+      } else if (option === textNode.options[3]) {
         button.addEventListener('click', () => slash())
-      }
-      else if (option === textNode.options[3]) {
+      } else if (option === textNode.options[4]) {
         button.addEventListener('click', () => slash())
-      }
-      else if (option === textNode.options[4]) {
-        button.addEventListener('click', () => slash())
-      }
-      else if (option === textNode.options[5]) {
+      } else if (option === textNode.options[5]) {
         button.addEventListener('click', () => slash())
       }
       optionButtonsElement.appendChild(button)
@@ -122,7 +122,7 @@ function showTextNode(textNodeIndex) { // goes through tthe text nodes checks wh
   })
 }
 
-function showOption(option) { //sho2ws the opitions if they have a certain item and such
+function showOption(option) { //shows the opitions if they have a certain item and such
   return option.requiredPlayer == null || option.requiredPlayer(player)
 }
 
@@ -141,23 +141,33 @@ function selectOption(option) { //decetcs if button is clicked with a set player
   showTextNode(nextTextNodeId)
 }
 
+function healPot(amount) {
+
+  
+
+}
 function slash() { //combat function only works if the combat funation is true and will only work if combat is on going or started 
 
   if (combatQuestion === true || continueCombat === true) {
-    console.log(`Yooooooooooooooooooooooo`)
     d12 = Math.floor(Math.random() * (13 - 1) + 1)
     damage = d12
-  }else {
+    attack = true
+  } else {
     console.log(`na man`)
+  }
+
+  //debug
+  if (player.debug >= 1) {
+    damage += 100
   }
 
   if (player.greatAxe >= 0 && player.str >= 7) {
     damage += 5
-  }else if (player.str >= 9) {
+  } else if (player.str >= 9) {
     damage += 4
   } else if (player.str >= 6) {
     damage += 2
-  }else if (player.str >= 3) {
+  } else if (player.str >= 3) {
     damage += 1
   }
 
@@ -165,13 +175,11 @@ function slash() { //combat function only works if the combat funation is true a
     damage += 3
   }
   damage = damage * 1.5
-  attack = true
-  // startCombat(enemy1, enemy1Name, enemy1Hp, enemy1Peace, enemy2, enemy2Name, enemy2Hp, enemy2Peace, enemy3, enemy3Name, enemy3Hp, enemy3Peace,)
+
+  combat()
 }
-
 // comstumizable combat system, it has evrything needed
-function startCombat(one, two, three, nextnode) {
-
+function startCombat() {
 
   if (player.con >= 9 && combatQuestion === true) {
     playerHp = 200
@@ -181,13 +189,106 @@ function startCombat(one, two, three, nextnode) {
     playerHp = 160
   } else { playerHp = 150 }
 
-
+  console.log(`It is in use`)
+  //debug
+  if (player.debug >= 1) {
+    playerHp = 690
+  }
 }
 
 function endCombat() {
+  if (playerHp <= 0) {
+    console.log(`yipee you are dead`)
+    startGame()
+  }
+  else if (enemy1.enemyHp <= 0 && enemy2.enemyHp <= 0 && enemy3.enemyHp <= 0 && combatQuestion === true) {
+    console.log(`YAY you are done`)
+    attack = false
+    combatQuestion = false
+    showTextNode(endingNode)
+
+  } else { console.log(`it did not work(end)`) }
+
 
 }
 
+function combat(one, two, three) {
+
+  if (one === 1) {
+    enemy1 = Bandit
+  } else if (one === 2) {
+    enemy1 = King
+  } else if (one === 3) {
+    enemy1 = None
+  }
+
+  if (two === 1) {
+    enemy2 = Bandit
+  } else if (two === 2) {
+    enemy2 = King
+  } else if (two === 3) {
+    enemy2 = None
+  }
+
+  if (three === 1) {
+    enemy3 = Bandit
+  } else if (three === 2) {
+    enemy3 = King
+  } else if (three === 3) {
+    enemy3 = None
+  }
+
+  battle1()
+  function battle1() {
+    if (attack === true && combatQuestion === true && enemy1.enemyHp >= .1) {
+
+      enemy1.enemyHp -= Math.floor(damage)
+
+      enemyD20 = Math.floor((Math.random() * (21 - 1) + 1) + enemy1.extraRoll)
+      enemyD20 = Math.floor((enemyD20 * 1.7) + enemy1.extraDamage)
+      playerHp -= Math.floor(enemyD20)
+      textElement.innerText = `The ${enemy1.enemyName} did ${enemyD20} damage to you which leaves you with <<${playerHp} HP>> left. You did ${damage} damage, leaving them with <${enemy1.enemyHp} HP> left.`
+      console.log(`The ${enemy1.enemyName} did ${enemyD20} damage to you which leaves you with ${playerHp} HP left. You did ${damage} damage, leaving them with ${enemy1.enemyHp} HP left.`)
+      damage = 0
+
+    }
+
+    else {
+      battle2()
+    }
+  }
+
+  function battle2() {
+    if (attack === true && combatQuestion === true && enemy2.enemyHp >= .1) {
+
+      enemy2.enemyHp -= Math.floor(damage)
+
+      enemyD20 = Math.floor((Math.random() * (21 - 1) + 1) + enemy2.extraRoll)
+      enemyD20 = Math.floor((enemyD20 * 1.7) + enemy2.extraDamage)
+      playerHp -= Math.floor(enemyD20)
+      textElement.innerText = `The ${enemy2.enemyName} did ${enemyD20} damage to you which leaves you with <<${playerHp} HP>> left. You did ${damage} damage, leaving them with <${enemy2.enemyHp} HP> left.`
+      console.log(`The ${enemy2.enemyName} did ${enemyD20} damage to you which leaves you with ${playerHp} HP left. You did ${damage} damage, leaving them with ${enemy2.enemyHp} HP left.`)
+      damage = 0
+
+    } else { battle3() }
+  }
+
+  function battle3() {
+    if (attack === true && combatQuestion === true && enemy3.enemyHp >= .1) {
+
+      enemy3.enemyHp -= Math.floor(damage)
+
+      enemyD20 = Math.floor((Math.random() * (21 - 1) + 1) + enemy3.extraRoll)
+      enemyD20 = Math.floor((enemyD20 * 1.7) + enemy3.extraDamage)
+      playerHp -= Math.floor(enemyD20)
+      textElement.innerText = `The ${enemy3.enemyName} did ${enemyD20} damage to you which leaves you with <<${playerHp} HP>> left. You did ${damage} damage, leaving them with <${enemy3.enemyHp} HP> left.`
+      console.log(`The ${enemy3.enemyName} did ${enemyD20} damage to you which leaves you with ${playerHp} HP left. You did ${damage} damage, leaving them with ${enemy3.enemyHp} HP left.`)
+      damage = 0
+
+    } else { endCombat() }
+  }
+
+}
 //holds all the story elements along with the options 
 let textNodes = [
   {
@@ -248,24 +349,24 @@ let textNodes = [
       }
     ],
   },
-  // {
-  //   id: 0,
-  //   text: `Our story begins in 40 AD on the newly conquered island of Britannia. Our protagonist is in a small village that seemed almost unaffected by the Roman invasion until one night when everything changed.`,
-  //   options: [
-  //     {
-  //       text: `More Info`,
-  //       nextText: 1
-  //     },
-  //     {
-  //       text: `Continue`,
-  //       nextText: 4
-  //     },
-  //     {
-  //       text: `Credits`,
-  //       nextText: 3
-  //     }
-  //   ],
-  // },
+  {
+    id: 8,
+    text: `Our story begins in 40 AD on the newly conquered island of Britannia. Our protagonist is in a small village that seemed almost unaffected by the Roman invasion until one night when everything changed.`,
+    options: [
+      {
+        text: `More Info`,
+        nextText: 1
+      },
+      {
+        text: `Continue`,
+        nextText: 9
+      },
+      {
+        text: `Credits`,
+        nextText: 3
+      }
+    ],
+  },
   {
     id: 4,
     text: `Currently battling`,
@@ -353,6 +454,37 @@ let textNodes = [
       }
     ],
 
+  },
+  {
+    id: 9,
+    text: `Currently battling id = 9`,
+    options: [
+      {
+        text: `Slash`,
+        nextText: 5
+      },
+      {
+        text: `Heal Potions`,
+        nextText: 2
+      },
+      {
+        text: `Scare`,
+        nextText: 2
+      },
+      {
+        text: `Stab`,
+        nextText: 2
+      },
+      {
+        text: `Pendant of Pain`,
+        nextText: 2
+      },
+      {
+        text: `Persuade`,
+        nextText: 2
+      }
+    ],
+    startCombat: 2 //depending on the number combat will change emenies or allies
   },
 ]
 startGame()
